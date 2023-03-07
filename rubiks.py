2208aa1c20b0dfd336ec878171a403c355b98865
Ahmad Daud <ahmad.daud.baloch@gmail.com>
2023-03-07 09:11:04 -0800

    Initial commit

---
+++ rubiks.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+cube = np.empty((6, 3, 3), dtype = 'U6')
+
+def indexing(letter):
+    side = np.empty(9, dtype = 'U6')
+    for index in np.arange(9):
+        side[index] = letter + str(side)
+    return side.reshape(3, 3)
+
+def reset():
+    letters = np.array(['B', 'R', 'G', 'O', 'Y', 'W'])
+    x = 0
+    for l in letters:
+        cube[x] = tagging(l)
+        x += 1
+    print('Cube Reset!')

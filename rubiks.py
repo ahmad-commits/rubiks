bd1cfb07e21d6b867bf24bd60d108a60798cb9f2
Ahmad Daud <ahmad.daud.baloch@gmail.com>
2023-03-08 07:30:02 -0800

    added comments

--- rubiks.py
+++ rubiks.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+#create empty 6 dimensional ndarray for 3x3 cube
 cube = np.empty((6, 3, 3), dtype = 'U6')
-
+#function to generate a 6x6 ndarray with the given letter indexes
 def indexing(letter):
     side = np.empty(9, dtype = 'U6')
     for index in np.arange(9):
         side[index] = letter + str(index)
     return side.reshape(3, 3)
-
+#reset the cube ndarray to default instances
 def reset():
     letters = np.array(['B', 'R', 'G', 'O', 'Y', 'W'])
     x = 0

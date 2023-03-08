9c331f75b60a3e8406fbd727248242abecddd8a7
Ahmad Daud <ahmad.daud.baloch@gmail.com>
2023-03-08 07:23:25 -0800

    Fixed cube generation issues

--- rubiks.py
+++ rubiks.py
@@ -5,13 +5,13 @@ cube = np.empty((6, 3, 3), dtype = 'U6')
 def indexing(letter):
     side = np.empty(9, dtype = 'U6')
     for index in np.arange(9):
-        side[index] = letter + str(side)
+        side[index] = letter + str(index)
     return side.reshape(3, 3)
 
 def reset():
     letters = np.array(['B', 'R', 'G', 'O', 'Y', 'W'])
     x = 0
     for l in letters:
-        cube[x] = tagging(l)
+        cube[x] = indexing(l)
         x += 1
     print('Cube Reset!')
